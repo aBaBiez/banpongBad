b39eaabac8f04db39a806371a869748b50795f2a
aBaBiez <phairotjzz@gmail.com>
2026-04-16 12:51:28 +0700

    again

--- enum/skillLevelEnum.ts
+++ enum/skillLevelEnum.ts
@@ -6,9 +6,9 @@ export enum SkillLevelEnum {
   S = "S",
   SPlus = "S+",
   PMinus = "P-",
-  // P = "P",
-  // X = "X",
-  // Unknown = "-"
+  P = "P",
+  X = "X",
+  Unknown = "-"
 }
 
 export const SkillLevelColor: Record<SkillLevelEnum, string> = {
@@ -19,7 +19,7 @@ export const SkillLevelColor: Record<SkillLevelEnum, string> = {
   [SkillLevelEnum.S]: "rgb(255, 255, 0)",       // yellow
   [SkillLevelEnum.SPlus]: "rgb(255, 166, 0)",   // orange
   [SkillLevelEnum.PMinus]: "rgb(255, 0, 0)"     // red
-  // [SkillLevelEnum.P]: "rgba(139, 0, 0, 0.5)",         // dark red
-  // [SkillLevelEnum.X]: "rgba(0, 0, 0, 0.5)",           // black
-  // [SkillLevelEnum.Unknown]: "rgba(128, 128, 128, 0.5)"// gray
+  [SkillLevelEnum.P]: "rgba(139, 0, 0, 0.5)",         // dark red
+  [SkillLevelEnum.X]: "rgba(0, 0, 0, 0.5)",           // black
+  [SkillLevelEnum.Unknown]: "rgba(128, 128, 128, 0.5)"// gray
 };
