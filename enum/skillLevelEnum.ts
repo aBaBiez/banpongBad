ec2018aba36ea81c2779ad7b6718e2a9e3de7ae6
aBaBiez <phairotjzz@gmail.com>
2026-04-16 12:53:14 +0700

    try

--- enum/skillLevelEnum.ts
+++ enum/skillLevelEnum.ts
@@ -11,15 +11,15 @@ export enum SkillLevelEnum {
   Unknown = "-"
 }
 
-export const SkillLevelColor: Record<SkillLevelEnum, string> = {
-  [SkillLevelEnum.BG1]: "rgb(128, 0, 128)",     // purple
-  [SkillLevelEnum.BG2]: "rgb(135, 206, 250)",   // light blue
-  [SkillLevelEnum.N]: "rgb(0, 0, 255)",         // blue
-  [SkillLevelEnum.NPlus]: "rgb(0, 128, 0)",     // green
-  [SkillLevelEnum.S]: "rgb(255, 255, 0)",       // yellow
-  [SkillLevelEnum.SPlus]: "rgb(255, 166, 0)",   // orange
-  [SkillLevelEnum.PMinus]: "rgb(255, 0, 0)"     // red
-  [SkillLevelEnum.P]: "rgba(139, 0, 0, 0.5)",         // dark red
-  [SkillLevelEnum.X]: "rgba(0, 0, 0, 0.5)",           // black
-  [SkillLevelEnum.Unknown]: "rgba(128, 128, 128, 0.5)"// gray
-};
+// export const SkillLevelColor: Record<SkillLevelEnum, string> = {
+//   [SkillLevelEnum.BG1]: "rgb(128, 0, 128)",     // purple
+//   [SkillLevelEnum.BG2]: "rgb(135, 206, 250)",   // light blue
+//   [SkillLevelEnum.N]: "rgb(0, 0, 255)",         // blue
+//   [SkillLevelEnum.NPlus]: "rgb(0, 128, 0)",     // green
+//   [SkillLevelEnum.S]: "rgb(255, 255, 0)",       // yellow
+//   [SkillLevelEnum.SPlus]: "rgb(255, 166, 0)",   // orange
+//   [SkillLevelEnum.PMinus]: "rgb(255, 0, 0)"     // red
+//   [SkillLevelEnum.P]: "rgba(139, 0, 0, 0.5)",         // dark red
+//   [SkillLevelEnum.X]: "rgba(0, 0, 0, 0.5)",           // black
+//   [SkillLevelEnum.Unknown]: "rgba(128, 128, 128, 0.5)"// gray
+// };
